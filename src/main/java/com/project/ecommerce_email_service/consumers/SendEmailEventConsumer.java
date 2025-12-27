8fccbf2d876f072dd94928c4f0a7a875878bcf97
TUAG-coder <agarwaltushar982@gmail.com>
2025-12-28 01:21:51 +0530

    feat: add functionality to send email to the user's email id after signup

--- src/main/java/com/project/ecommerce_email_service/consumers/SendEmailEventConsumer.java
+++ src/main/java/com/project/ecommerce_email_service/consumers/SendEmailEventConsumer.java
@@ -3,8 +3,15 @@ package com.project.ecommerce_email_service.consumers;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.project.ecommerce_email_service.dtos.SendEmailDto;
+import com.project.ecommerce_email_service.utils.EmailUtil;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
+
+import javax.mail.Authenticator;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import java.util.Properties;
 
 @Service
 public class SendEmailEventConsumer {
@@ -14,13 +21,36 @@ public class SendEmailEventConsumer {
         this.objectMapper = objectMapper;
     }
 
+    // "sendEmail" is the topic name where UserService publishes event after user signup
     @KafkaListener(topics = "sendEmail", groupId = "emailService")
     public void handleSendEmailEvent(String message) throws JsonProcessingException {
         SendEmailDto sendEmailDto = this.objectMapper.readValue(message, SendEmailDto.class);
 
-        String to = sendEmailDto.getTo();
-        String from = sendEmailDto.getFrom();
+        String toEmail = sendEmailDto.getToEmail();
+        String fromEmail = sendEmailDto.getFromEmail();
         String subject = sendEmailDto.getSubject();
         String body = sendEmailDto.getBody();
+
+        Properties props = new Properties();
+        props.put("mail.smtp.host", "smtp.gmail.com"); //SMTP Host
+        props.put("mail.smtp.port", "587"); //TLS Port
+        props.put("mail.smtp.auth", "true"); //enable authentication
+        props.put("mail.smtp.starttls.enable", "true"); //enable STARTTLS
+
+        //create Authenticator object to pass in Session.getInstance argument
+        Authenticator auth = new Authenticator() {
+            //override the getPasswordAuthentication method
+
+            /*
+            It's not good practice to put Gmail account password, so I have created
+            an App password for "fromEmail" gmail account under EmailService app name
+             */
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(fromEmail, "rhhbsgrkqfdcyqav");
+            }
+        };
+        Session session = Session.getInstance(props, auth);
+
+        EmailUtil.sendEmail(session, toEmail, subject, body);
     }
 }
